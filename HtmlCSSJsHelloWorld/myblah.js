4d5b26736028a986adc313c27c72ad32909edc6a
Shantanu Pandey <shantanu.pandey@live.in>
2022-06-20 22:42:00 +0800

    Add files via upload

---
+++ HtmlCSSJsHelloWorld/myblah.js
@@ -0,0 +1,13 @@
+function randomFunction(){
+                                alert("shouldn't have clicked me");
+                        }
+
+
+
+function randomfunk2(a, b){
+
+return a+b;
+
+}
+
+
